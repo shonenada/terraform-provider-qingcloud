bf0602c52986e6c502b25842f078bf60ccfe5abd
shonenada <shonenada@gmail.com>
2017-09-27 11:41:14 +0800

    [base]: Fork from template, replace some docs

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/shonenada/terraform-provider-qingcloud/qingcloud"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: template.Provider})
+}
