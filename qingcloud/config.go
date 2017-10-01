9658d0aebc60b930e53c6619cdaf801128eaf97a
shonenada <shonenada@gmail.com>
2017-10-01 20:47:28 +0800

    [config]: add config.go

---
+++ qingcloud/config.go
@@ -0,0 +1,38 @@
+package qingcloud
+
+import (
+	"log"
+
+	qcConfig "github.com/yunify/qingcloud-sdk-go/config"
+	qcService "github.com/yunify/qingcloud-sdk-go/service"
+)
+
+type Config struct {
+	AccessKey string
+	SecretKey string
+}
+
+type QingCloudClient struct {
+	config  *Config
+	service *qcService.QingCloudService
+}
+
+func (c *Config) Client() (*QingCloudClient, error) {
+	cfg, err := qcConfig.New(c.AccessKey, c.SecretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	srv, err := qcService.Init(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("[INFO] Qingcloud Client created")
+
+	return &QingCloudClient{
+		config:  c,
+		service: srv,
+	}, nil
+}
